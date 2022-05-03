a51993f021cc7a2e95df05a3f88ccdd7b7030b9e
dunggin <dungho451@gmail.com>
2022-05-03 16:21:12 +0700

    edit config

--- udagram-api-user/src/config/config.ts
+++ udagram-api-user/src/config/config.ts
@@ -1,37 +1,14 @@
-// export const config = {
-//   'username': process.env.POSTGRES_USERNAME,
-//   'password': 'adminadmin',
-//   'database': process.env.POSTGRES_DB,
-//   'host': 'database-1.clrxzcplzyra.us-east-1.rds.amazonaws.com',
-//   'dialect': 'postgres',
-//   'aws_region': process.env.AWS_REGION,
-//   'aws_profile': process.env.AWS_PROFILE,
-//   'aws_media_bucket': process.env.AWS_BUCKET,
-//   'url': process.env.URL,
-//   'jwt': {
-//     'secret': process.env.JWT_SECRET,
-//   },
-// };
 export const config = {
-  'username': 'root',
-  'password': 'adminadmin',
-  'database': 'postgres',
-  'host': 'database-1.clrxzcplzyra.us-east-1.rds.amazonaws.com',
+  'username': process.env.POSTGRES_USERNAME,
+  'password': 'process.env.POSTGRES_PASSWORD',
+  'database': process.env.POSTGRES_DB,
+  'host': 'process.env.POSTGRES_HOST',
   'dialect': 'postgres',
-  'aws_region': 'us-east-1',
-  'aws_profile': 'default',
-  'aws_media_bucket': 'arn:aws:s3:::test-636561002921-app3',
-  'url': 'http://localhost:8100',
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
+  'url': process.env.URL,
   'jwt': {
-    'secret': 'testing',
+    'secret': process.env.JWT_SECRET,
   },
 };
-// POSTGRES_USERNAME=postgres
-// POSTGRES_PASSWORD=adminadmin
-// POSTGRES_HOST=database-1.clrxzcplzyra.us-east-1.rds.amazonaws.com
-// POSTGRES_DB=postgres
-// AWS_BUCKET=arn:aws:s3:::test-636561002921-app3
-// AWS_REGION=us-east-1
-// AWS_PROFILE=default
-// JWT_SECRET=testing
-// URL=http://localhost:8100
